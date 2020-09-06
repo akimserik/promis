5105cb42da421261324406d7c12797c40c33f52b
Serik Akimgereyev <s.akimgereyev@gmail.com>
2020-09-06 18:26:05 +0600

    app

--- app.js
+++ app.js
@@ -79,7 +79,7 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static(`${__dirname}/public`));
 
   // Handle SPA
-  // app.get(/.*/, (req, res) => res.sendFile(`${__dirname}/public/index.html`));
+  app.get('/', (req, res) => res.end(`${__dirname}/public/index.html`));
 }
 
 app.use(globalErrorHandler);
